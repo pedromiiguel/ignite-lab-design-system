a2f79d895fa8123bef8eb5cae5dbb6e3a8684a39
Pedro Miguel <pedromrap@gmail.com>
2022-10-13 16:05:26 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
