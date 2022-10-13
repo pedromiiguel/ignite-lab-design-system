14412e5f0a4afe7ce1475fd0d8856cb80d50aa36
Pedro Miguel <pedromrap@gmail.com>
2022-10-13 17:58:02 -0300

    feat: create text input component

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Envelope } from 'phosphor-react';
+import { TextInput, TextInputRootProps } from '.';
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />,
+    ],
+  },
+  argTypes: {},
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputRootProps> = {
+  argTypes: {
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [<TextInput.Input placeholder="Type your e-mail address" />],
+  },
+  argTypes: {
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        black: '#000000',
+        white: '#FFFFFF',
         'gray-900': '#121214',
         'gray-800': '#202024',
         'gray-400': '#7C7C8A',
