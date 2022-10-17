a5d8cf1482344ca633b9931d6d5846408c729f35
Pedro Miguel <pedromrap@gmail.com>
2022-10-17 14:41:12 -0300

    feat(Logo): create logo component

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import { Button } from './Button';
+import { Checkbox } from './Checkbox';
+import { TextInput } from './TextInput';
+import { Heading } from './Heading';
+import { Text } from './Text';
+import { Logo } from './Logo';
+
+export { Button, Checkbox, Heading, Text, Logo, TextInput };
