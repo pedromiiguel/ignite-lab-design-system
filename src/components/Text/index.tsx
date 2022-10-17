65e3f9152a800c6b78a804c7e329c8c5947a4d71
Pedro Miguel <pedromrap@gmail.com>
2022-10-17 14:40:41 -0300

    feat(Text): add new props

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -6,9 +6,10 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Text({ size = 'md', children, asChild }: TextProps) {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const Component = asChild ? Slot : 'span';
 
   const sizes = {
@@ -18,7 +19,9 @@ export function Text({ size = 'md', children, asChild }: TextProps) {
   };
 
   return (
-    <Component className={clsx('text-gray-100 font-sans', sizes[size])}>
+    <Component
+      className={clsx('text-gray-100 font-sans', sizes[size], className)}
+    >
       {children}
     </Component>
   );
