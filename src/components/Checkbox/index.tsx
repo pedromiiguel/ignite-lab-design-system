79aef59be35c149dae92bc742f5b5dd5714a7f23
Pedro Miguel <pedromrap@gmail.com>
2022-10-17 14:40:08 -0300

    feat(Checkbox): add new props

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -2,17 +2,18 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { Text } from '../Text';
 
-export interface CheckboxProps {
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {
   label: string;
   id: string;
 }
 
-export function Checkbox({ label, id }: CheckboxProps) {
+export function Checkbox({ label, id, ...rest }: CheckboxProps) {
   return (
     <div className="flex items-center gap-2">
       <CheckboxPrimitive.Root
         className="w-6 h-6 p-[2px] bg-gray-800 rounded"
         id={id}
+        {...rest}
       >
         <CheckboxPrimitive.Indicator>
           <Check weight="bold" className="w-5 h-5 text-cyan-500" />
