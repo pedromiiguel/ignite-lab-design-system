c8235cfd96a6e1c940d11d32f11e0f007ba70e6f
Pedro Miguel <pedromrap@gmail.com>
2022-10-24 10:46:43 -0300

    chore: add mock service worker

--- src/pages/Signin.stories.tsx
+++ src/pages/Signin.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, waitFor } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { rest } from 'msw';
 import { SignIn } from './Signin';
 
 export default {
@@ -8,6 +9,15 @@ export default {
   component: SignIn,
   args: {},
   argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(ctx.json({ message: 'Login realizado!' }));
+        }),
+      ],
+    },
+  },
 } as Meta;
 
 export const Default: StoryObj = {
@@ -25,5 +35,32 @@ export const Default: StoryObj = {
     await waitFor(() => {
       expect(canvas.getByText('Login realizado!'));
     });
+  },
+};
+
+export const WithError: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    userEvent.type(
+      canvas.getByPlaceholderText('Digite seu e-mail'),
+      'pedro@email.com'
+    );
+    userEvent.type(canvas.getByPlaceholderText('******'), '123456');
+
+    userEvent.click(canvas.getByRole('button'));
+
+    await waitFor(() => {
+      expect(canvas.getByText('Ocorreu um erro ao realizar o login.'));
+    });
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/sessions', (req, res, ctx) => {
+          return res(ctx.status(404, 'Ocorreu um erro ao realizar o login.'));
+        }),
+      ],
+    },
   },
 };
